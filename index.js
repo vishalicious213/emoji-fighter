32423a01c24dbfcf8c0da4df8362e845b36c0247
vishalicious213 <vishalicious213@gmail.com>
2023-01-04 00:03:43 -0800

    randomized background image and added 4 images

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "
 let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
+function randomBackground() {
+    let imgNum = Math.floor((Math.random() * 4) + 1)
+    const background = document.getElementById("background")
+    background.style.backgroundImage = `url("img/0${imgNum}.jpg")`
+    background.style.backgroundRepeat = "no-repeat"
+    background.style.backgroundSize = "cover"
+    background.style.backgroundPosition = "center"
+    background.style.backgroundAttachment = "fixed"
+}
+
 fightButton.addEventListener("click", function() {
     let player1 = Math.floor(Math.random() * 17)
     let player2 = Math.floor(Math.random() * 17)
     stageEl.textContent = `${fighters[player1]} vs ${fighters[player2]}`
 })
+
+randomBackground()
